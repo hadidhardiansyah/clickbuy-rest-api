ba91ee30edd73f53332189561e1d90af6c5a8bdd
Hadid Hardiansyah <hadidhardiansyah@gmail.com>
2025-02-22 21:13:51 +0700

    Create project and configura application properties

---
+++ src/main/java/com/hadid/clickbuy/restapi/ClickBuyRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.hadid.clickbuy.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class ClickBuyRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickBuyRestApiApplication.class, args);
+	}
+
+}
